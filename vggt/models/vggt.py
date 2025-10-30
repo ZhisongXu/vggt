af1112a3543d076aacfeac010e80733ee06ec306
ZhisongXu <zhisongxv@gmail.com>
2025-10-31 00:12:07 +0900

    vggt

--- vggt/models/vggt.py
+++ vggt/models/vggt.py
@@ -8,11 +8,10 @@ import torch
 import torch.nn as nn
 from huggingface_hub import PyTorchModelHubMixin  # used for model hub
 
-from vggt.models.aggregator import Aggregator
-from vggt.heads.camera_head import CameraHead
-from vggt.heads.dpt_head import DPTHead
-from vggt.heads.track_head import TrackHead
-
+from vggt.vggt.models.aggregator import Aggregator
+from vggt.vggt.heads.camera_head import CameraHead
+from vggt.vggt.heads.dpt_head import DPTHead
+from vggt.vggt.heads.track_head import TrackHead
 
 class VGGT(nn.Module, PyTorchModelHubMixin):
     def __init__(self, img_size=518, patch_size=14, embed_dim=1024,
